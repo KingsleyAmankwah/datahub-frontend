fba766d214ec259b2fcfb0c90af3a2620ab5f486
KingsleyAmankwah <kuamiklassic149@gmail.com>
2026-04-05 12:50:17 +0000

    refactor: update proxy with public routes

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
-const PUBLIC_ROUTES = ["/login"];
+const PUBLIC_ROUTES = ["/login", "/store"];
 
 export function proxy(req: NextRequest) {
   const { pathname } = req.nextUrl;
